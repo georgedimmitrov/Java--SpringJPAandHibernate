f8d00c7431f504c5d6a71c8d355dbf75306d28ed
Georgi Dimitrov <dgeorgi@vmware.com>
2018-08-08 16:17:04 +0100

    jpa course

---
+++ src/main/java/com/pluralsight/model/Exercise.java
@@ -0,0 +1,61 @@
+package com.pluralsight.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+@Table(name = "exercise")
+public class Exercise {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Range(min = 1, max = 120)
+	private int minutes;
+
+	@NotNull
+	private String activity;
+
+	@ManyToOne
+	private Goal goal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+	public String getActivity() {
+		return activity;
+	}
+
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+
+    public Goal getGoal() {
+        return goal;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+	
+}
